6a45261593e205a995ea1792a31f956a1f2932af
No-orja <nomicro12311@gmail.com>
2025-04-08 12:10:33 +0300

    update api base url

--- src/Api/BaseUrl.js
+++ src/Api/BaseUrl.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
-const baseURL = axios.create({ baseURL: 'http://localhost:5001/' });
-
+const baseURL = axios.create({
+    baseURL: 'https://server-jet-three-70.vercel.app/', // Vercel server URL
+    // baseURL: 'http://localhost:5000/', // Local server URL
+  });
 export default baseURL;
+
+
+
